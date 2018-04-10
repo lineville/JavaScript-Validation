5da7822c4cae810ee071a4c586c9be2d708f4108
Liam Neville <lineville@vassar.edu>
2018-04-09 23:08:11 -0400

    name, email and password tests added

--- validate.js
+++ validate.js
@@ -221,6 +221,12 @@ function genericDate() {
   var year = date.getFullYear();
   var month = date.getMonth() + 1;
   var day = date.getDate();
+  if(day < 10){
+    day = "0" + day;
+  }
+    if(month < 10){
+        month = "0" + month;
+    }
   data.value = year + "/" + month + "/" + day;
   //console.log("focus out");
 
